048d378b1be66c8bb08b4fbee6c67acce7f26827
Jonathan Bridge <jvbridge@gmail.com>
2022-04-27 19:45:28 -0700

    empty get and post

---
+++ routes/notes.js
@@ -0,0 +1,10 @@
+const nt = require('express').Router();
+const {readDb, writeDb, appendDb} = require('../lib/fsReadWrite');
+
+nt.get('/', (req, res) => {
+
+});
+
+nt.post('/', (req, res) => {
+
+});
